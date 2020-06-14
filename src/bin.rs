242dc0d3f959c3b2e774d553e151e2d06f7a0424
Douglas Anderson <hockeybuggy@gmail.com>
2020-06-14 14:25:43 -0400

    Don't throw an error if there are no tasks.

--- src/bin.rs
+++ src/bin.rs
@@ -1,11 +1,10 @@
-use std::process;
-
 use chrono::{DateTime, Utc};
 use chrono_tz::Tz;
 use recurring_tasks::{display_upcoming_tasks, get_tasks_occurring_in_the_next_hour, Task};
 
 fn main() {
     // TODO I hope to make this an external file that can be read in.
+    let local_timezone: Tz = "America/Toronto".parse().unwrap();
     let tasks: Vec<Task> = vec![
         Task {
             cron_expression: "0 0 3 * * * *".to_owned(),
@@ -49,18 +48,16 @@ fn main() {
         },
     ];
 
-    let local_timezone: Tz = "America/Toronto".parse().unwrap();
     let now: DateTime<Utc> = Utc::now();
     let local_datetime = now.with_timezone(&local_timezone);
-    println!("it is now {:?} in utc", now);
-    println!("it is now {:?} in {:?}", local_datetime, local_timezone);
+    println!("it is now {:?} in UTC", now);
+    println!("      and {:?} in {:?}", local_datetime, local_timezone);
 
     let upcoming = get_tasks_occurring_in_the_next_hour(&tasks, local_datetime);
 
     if upcoming.len() == 0 {
         println!("There are no upcoming tasks.");
-        process::exit(1);
+    } else {
+        display_upcoming_tasks(&upcoming);
     }
-
-    display_upcoming_tasks(&upcoming);
 }
