35a7fa5c3db9b70650f2b0a28073b655511859d4
Douglas Anderson <hockeybuggy@gmail.com>
2020-06-16 21:25:20 -0400

    Round the time to the beginning of the day.
    This now means that it will be sending messages for all of the tasks in
    the local datetime.
    
    In this I also rename the executable's argument to `tasks`.

--- src/bin.rs
+++ src/bin.rs
@@ -1,46 +1,30 @@
 use std::path::Path;
 
 extern crate clap;
-use chrono::{DateTime, Utc};
 use clap::{App, Arg};
 
-use recurring_tasks::{
-    display_upcoming_tasks, get_tasks_occurring_within_duration, parse_toml_file,
-};
+use recurring_tasks::run_from_task_file;
 
 const ABOUT_BLERB: &str = "
 This project outputs a task list given a schedule.
 ";
 
 fn main() {
-    let matches = App::new("ValueType Codegen")
+    let matches = App::new("Recurring tasks")
         .version("0.1.0")
         .author("Douglas Anderson <hockeybuggy@gmail.com>")
         .about(ABOUT_BLERB)
         .arg(
-            Arg::with_name("source")
-                .help("Sets the input file to use")
+            Arg::with_name("tasks")
+                .help("Sets the input task file to use")
                 .takes_value(true)
-                .short("s")
-                .long("source")
+                .short("t")
+                .long("tasks")
                 .required(true),
         )
         .get_matches();
 
-    let source_path = Path::new(matches.value_of("source").unwrap());
-    let (timezone, tasks) = parse_toml_file(source_path).unwrap();
+    let source_path = Path::new(matches.value_of("tasks").unwrap());
 
-    let now: DateTime<Utc> = Utc::now();
-    let local_datetime = now.with_timezone(&timezone);
-
-    let day = chrono::Duration::days(1);
-    let upcoming = get_tasks_occurring_within_duration(&tasks, local_datetime, day);
-
-    println!(
-        "Tasks between:\n\t- {:?}\n\t- {:?}",
-        local_datetime,
-        local_datetime + day
-    );
-    println!("");
-    display_upcoming_tasks(&upcoming);
+    run_from_task_file(source_path);
 }
