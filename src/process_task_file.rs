55fdb3b8a3ecd9340f30d3f874cf6f8b5ef0fa80
Douglas Anderson <hockeybuggy@gmail.com>
2020-06-20 15:32:47 -0400

    Start writing tests for processing task files
    Right now there isn't much there other than setting up a valid test
    case. My plan is to next start chipping away at removing the many
    unwraps in the function.

--- src/process_task_file.rs
+++ src/process_task_file.rs
@@ -43,3 +43,59 @@ pub fn parse_toml_file(source_path: &Path) -> Result<(chrono_tz::Tz, Vec<Task>),
     }
     return Ok((local_timezone, tasks));
 }
+
+#[cfg(test)]
+mod process_task_file_tests {
+    use std::env;
+    use std::fs::File;
+    use std::io::prelude::*;
+    use std::path::PathBuf;
+
+    use crate::process_task_file::parse_toml_file;
+    use crate::tasks::Task;
+
+    fn write_to_tempfile(file_name: &str, file_contents: &str) -> PathBuf {
+        let dir = env::temp_dir();
+        let source_path = dir.join(file_name);
+        let mut temp_file = File::create(&source_path).unwrap();
+        temp_file.write_all(&file_contents.as_bytes()).unwrap();
+
+        return source_path;
+    }
+
+    #[test]
+    fn test_parse_toml_file_valid_file() {
+        let source_path = write_to_tempfile(
+            "valid_simple_example.toml",
+            "
+timezone = 'America/Toronto'
+
+[tasks]
+
+[tasks.water]
+name = 'Drink water (min 1 litre)'
+cron_expression = '0 0 * * * * *'
+
+[tasks.teeth]
+name = 'Brush teeth'
+cron_expression = '0 0 * * * * *'
+",
+        );
+
+        let (local_timezone, tasks) = parse_toml_file(&source_path).unwrap();
+
+        let expected_timezone = chrono_tz::America::Toronto;
+        assert_eq!(local_timezone, expected_timezone);
+        let expected_tasks: Vec<Task> = vec![
+            Task {
+                cron_expression: "0 0 * * * * *".to_owned(),
+                task_name: "Brush teeth".to_owned(),
+            },
+            Task {
+                cron_expression: "0 0 * * * * *".to_owned(),
+                task_name: "Drink water (min 1 litre)".to_owned(),
+            },
+        ];
+        assert_eq!(tasks, expected_tasks);
+    }
+}
