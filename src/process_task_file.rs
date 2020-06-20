def15ed497fd683eb72f4010ee2ed9a124505972
Douglas Anderson <hockeybuggy@gmail.com>
2020-06-20 16:28:39 -0400

    Improve the parsing of timezones and allow other task table names.
    This commit tests the behaviour of timezones and removes some of the
    `unwrap`s with `Err` returns.
    
    This commit also changes the parsing of the input file to allow more
    than one table of tasks.
    
    Closes: #14

--- src/process_task_file.rs
+++ src/process_task_file.rs
@@ -13,33 +13,47 @@ pub fn parse_toml_file(source_path: &Path) -> Result<(chrono_tz::Tz, Vec<Task>),
         Err(error) => return Err(format!("Could not parse toml: {}", error)),
     };
 
-    // TODO This parsing could be better.
-    let local_timezone: Tz = parsed
-        .get("timezone")
-        .unwrap()
-        .as_str()
-        .unwrap()
-        .parse()
-        .unwrap();
+    let maybe_timezone = parsed.get("timezone");
+    if maybe_timezone.is_none() {
+        return Err("Missing required `timezone` field.".to_owned());
+    }
+    let maybe_timezone_string = maybe_timezone.unwrap().as_str();
+    if maybe_timezone_string.is_none() {
+        return Err(
+            "`timezone` field should be a string containing a valid timezone name.".to_owned(),
+        );
+    }
+    let local_timezone: Tz = maybe_timezone_string.unwrap().parse()?;
 
     let mut tasks: Vec<Task> = vec![];
 
-    let raw_tasks = parsed.get("tasks").unwrap();
-    for (_task_name, task_table) in raw_tasks.as_table().unwrap() {
-        tasks.push(Task {
-            task_name: task_table
-                .get("name")
-                .unwrap()
-                .as_str()
-                .unwrap()
-                .to_string(),
-            cron_expression: task_table
-                .get("cron_expression")
-                .unwrap()
-                .as_str()
-                .unwrap()
-                .to_string(),
-        });
+    let tables: Vec<(&String, &Toml)> = parsed
+        .as_table()
+        .unwrap()
+        .iter()
+        .filter(|key_value_pair| key_value_pair.1.is_table())
+        .collect();
+    if tables.len() == 0 {
+        return Err("Could not find tasks.".to_owned());
+    }
+
+    for tasks_table in tables {
+        for (_task_name, task_table) in tasks_table.1.as_table().unwrap() {
+            tasks.push(Task {
+                task_name: task_table
+                    .get("name")
+                    .unwrap()
+                    .as_str()
+                    .unwrap()
+                    .to_string(),
+                cron_expression: task_table
+                    .get("cron_expression")
+                    .unwrap()
+                    .as_str()
+                    .unwrap()
+                    .to_string(),
+            });
+        }
     }
     return Ok((local_timezone, tasks));
 }
@@ -70,8 +84,6 @@ mod process_task_file_tests {
             "
 timezone = 'America/Toronto'
 
-[tasks]
-
 [tasks.water]
 name = 'Drink water (min 1 litre)'
 cron_expression = '0 0 * * * * *'
@@ -97,5 +109,121 @@ cron_expression = '0 0 * * * * *'
             },
         ];
         assert_eq!(tasks, expected_tasks);
+    }
+
+    #[test]
+    fn test_parse_toml_file_multiple_task_tables() {
+        let source_path = write_to_tempfile(
+            "multiple_task_tables.toml",
+            "
+timezone = 'America/Toronto'
+
+[tasks.water]
+name = 'Drink water (min 1 litre)'
+cron_expression = '0 0 * * * * *'
+
+[birthdays.neo]
+name = 'Thomas Anderson Birthday'
+cron_expression = '0 0 * 3 11 * *'
+",
+        );
+
+        let (local_timezone, tasks) = parse_toml_file(&source_path).unwrap();
+
+        let expected_timezone = chrono_tz::America::Toronto;
+        assert_eq!(local_timezone, expected_timezone);
+        let expected_tasks: Vec<Task> = vec![
+            Task {
+                cron_expression: "0 0 * 3 11 * *".to_owned(),
+                task_name: "Thomas Anderson Birthday".to_owned(),
+            },
+            Task {
+                cron_expression: "0 0 * * * * *".to_owned(),
+                task_name: "Drink water (min 1 litre)".to_owned(),
+            },
+        ];
+        assert_eq!(tasks, expected_tasks);
+    }
+
+    #[test]
+    fn test_parse_toml_file_missing_timezone() {
+        let source_path = write_to_tempfile(
+            "missing_timezone.toml",
+            "
+# This task file is mising the required `timezone` field.
+
+[tasks]
+
+[tasks.water]
+name = 'Drink water (min 1 litre)'
+cron_expression = '0 0 * * * * *'
+",
+        );
+
+        let err = parse_toml_file(&source_path).unwrap_err();
+
+        let expected_err = "Missing required `timezone` field.";
+        assert_eq!(err, expected_err);
+    }
+
+    #[test]
+    fn test_parse_toml_file_timezone_of_wrong_type() {
+        let source_path = write_to_tempfile(
+            "timezone_of_wrong_type.toml",
+            "
+# This task file has `timezone` field of the wrong type. It should be a string.
+timezone = 1
+
+[tasks]
+
+[tasks.water]
+name = 'Drink water (min 1 litre)'
+cron_expression = '0 0 * * * * *'
+",
+        );
+
+        let err = parse_toml_file(&source_path).unwrap_err();
+
+        let expected_err = "`timezone` field should be a string containing a valid timezone name.";
+        assert_eq!(err, expected_err);
+    }
+
+    #[test]
+    fn test_parse_toml_file_timezone_that_does_not_exist() {
+        let source_path = write_to_tempfile(
+            "invalid_timezone.toml",
+            "
+# This task file has `timezone` field that doesn't exist in the timezone db
+timezone = 'Pangaea/FutureToronto'
+
+[tasks]
+
+[tasks.water]
+name = 'Drink water (min 1 litre)'
+cron_expression = '0 0 * * * * *'
+",
+        );
+
+        let err = parse_toml_file(&source_path).unwrap_err();
+
+        let expected_err = "'Pangaea/FutureToronto\' is not a valid timezone";
+        assert_eq!(err, expected_err);
+    }
+
+    #[test]
+    fn test_parse_toml_file_missing_tasks_table() {
+        let source_path = write_to_tempfile(
+            "missing_tasks_table.toml",
+            "
+timezone = 'America/Toronto'
+
+# This task file doesn't have a Table containing tasks
+",
+        );
+
+        let err = parse_toml_file(&source_path).unwrap_err();
+
+        let expected_err = "Could not find tasks.";
+        assert_eq!(err, expected_err);
     }
 }
